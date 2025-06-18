56bdc6df6f3e4d211b7d3b40eff1cce0d27f4719
Felipe Chávez <136279896+Chavezgarrido@users.noreply.github.com>
2025-06-17 23:47:12 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
+require('dns').setDefaultResultOrder('ipv4first'); 
+
 const { Sequelize } = require('sequelize');
 
-const password = String(process.env.DB_PASSWORD);  
+const password = String(process.env.DB_PASSWORD);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  password,         
+  password,
   {
     host: process.env.DB_HOST,
     port: 5432,
