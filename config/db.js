dce30a532e4d8a2bcf0cfec96184ba0f671351c9
Felipe Chávez <136279896+Chavezgarrido@users.noreply.github.com>
2025-06-18 00:01:59 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,26 +1,19 @@
 require('dotenv').config();
-require('dns').setDefaultResultOrder('ipv4first'); 
+require('dns').setDefaultResultOrder('ipv4first');
 
 const { Sequelize } = require('sequelize');
 
-const password = String(process.env.DB_PASSWORD);
+const databaseUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  password,
-  {
-    host: process.env.DB_HOST,
-    port: 5432,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-);
+});
 
 module.exports = sequelize;
